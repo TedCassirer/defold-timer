86c9bc8fb583e1d0e053e6c93dbe5de2b5f8cece
Björn Ritzl <bjorn.ritzl@king.com>
2017-03-21 07:14:52 +0100

    Added timer code and example

---
+++ timer/src/timer.cpp
@@ -0,0 +1,168 @@
+// copyright britzl
+// Extension lib defines
+#define LIB_NAME "Timer"
+#define MODULE_NAME "timer"
+
+// include the Defold SDK
+#include <dmsdk/sdk.h>
+#include <sys/time.h>
+
+struct Listener {
+  Listener() {
+    m_L = 0;
+    m_Callback = LUA_NOREF;
+    m_Self = LUA_NOREF;
+  }
+  lua_State* m_L;
+  int        m_Callback;
+  int        m_Self;
+};
+
+struct Timer {
+  double seconds;
+  double end;
+  unsigned int id;
+  Listener listener;
+};
+
+static unsigned int sequenceId = 0;
+static const int TIMERS_CAPACITY = 128;
+static dmArray<Timer*> timers;
+
+
+/**
+ * Create a listener instance from a function on the stack
+ */
+static Listener CreateListener(lua_State* L, int index) {
+  luaL_checktype(L, index, LUA_TFUNCTION);
+  lua_pushvalue(L, index);
+  int cb = dmScript::Ref(L, LUA_REGISTRYINDEX);
+
+  Listener listener;
+  listener.m_L = dmScript::GetMainThread(L);
+  listener.m_Callback = cb;
+  dmScript::GetInstance(L);
+  listener.m_Self = dmScript::Ref(L, LUA_REGISTRYINDEX);
+  return listener;
+}
+
+/**
+ * Get a timestamp in milliseconds
+ */
+static double GetTimestamp() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+}
+
+static int Seconds(lua_State* L) {
+  int top = lua_gettop(L);
+
+  double seconds = luaL_checknumber(L, 1);
+
+  Timer *timer = (Timer*)malloc(sizeof(Timer));
+  timer->listener = CreateListener(L, 2);
+  timer->seconds = seconds;
+  timer->id = sequenceId++;
+  timer->end = GetTimestamp() + seconds * 1000.0f;
+
+  if (timers.Full()) {
+    timers.SetCapacity(timers.Capacity() + TIMERS_CAPACITY);
+  }
+  timers.Push(timer);
+
+  lua_pushinteger(L, timer->id);
+
+  assert(top + 1 == lua_gettop(L));
+  return 1;
+}
+
+
+static int Cancel(lua_State* L) {
+  int top = lua_gettop(L);
+
+  int id = luaL_checkint(L, 1);
+  for (int i = timers.Size() - 1; i >= 0; i--) {
+    Timer* timer = timers[i];
+    if (timer->id == id) {
+      timers.EraseSwap(i);
+      free(timer);
+      break;
+    }
+  }
+  assert(top + 0 == lua_gettop(L));
+  return 0;
+}
+
+// Functions exposed to Lua
+static const luaL_reg Module_methods[] = {
+  { "seconds", Seconds },
+  { "cancel", Cancel },
+  { 0, 0 }
+};
+
+static void LuaInit(lua_State* L) {
+  int top = lua_gettop(L);
+
+  luaL_register(L, MODULE_NAME, Module_methods);
+
+  lua_pop(L, 1);
+  assert(top == lua_gettop(L));
+}
+
+dmExtension::Result AppInitializeExtension(dmExtension::AppParams* params) {
+  return dmExtension::RESULT_OK;
+}
+
+dmExtension::Result InitializeExtension(dmExtension::Params* params) {
+  LuaInit(params->m_L);
+  printf("Registered %s Extension\n", MODULE_NAME);
+  return dmExtension::RESULT_OK;
+}
+
+dmExtension::Result AppFinalizeExtension(dmExtension::AppParams* params) {
+  return dmExtension::RESULT_OK;
+}
+
+dmExtension::Result UpdateExtension(dmExtension::Params* params) {
+  const double now = GetTimestamp();
+  int i = 0;
+  for (int i = timers.Size() - 1; i >= 0; i--) {
+    Timer* timer = timers[i];
+    if (now >= timer->end) {
+      lua_State* L = timer->listener.m_L;
+      int top = lua_gettop(L);
+
+      lua_rawgeti(L, LUA_REGISTRYINDEX, timer->listener.m_Callback);
+      lua_rawgeti(L, LUA_REGISTRYINDEX, timer->listener.m_Self);
+      lua_pushvalue(L, -1);
+      dmScript::SetInstance(L);
+      if (!dmScript::IsInstanceValid(L)) {
+        lua_pop(L, 2);
+      } else {
+        lua_pushinteger(L, timer->id);
+        int ret = lua_pcall(L, 2, LUA_MULTRET, 0);
+        if (ret != 0) {
+            dmLogError("Error running timer callback: %s", lua_tostring(L, -1));
+            lua_pop(L, 1);
+        }
+      }
+      assert(top == lua_gettop(L));
+
+      timers.EraseSwap(i);
+      free(timer);
+    }
+  }
+  return dmExtension::RESULT_OK;
+}
+
+dmExtension::Result FinalizeExtension(dmExtension::Params* params) {
+  return dmExtension::RESULT_OK;
+}
+
+
+// Defold SDK uses a macro for setting up extension entry points:
+//
+// DM_DECLARE_EXTENSION(symbol, name, app_init, app_final, init, update, on_event, final)
+
+DM_DECLARE_EXTENSION(Timer, LIB_NAME, AppInitializeExtension, AppFinalizeExtension, InitializeExtension, UpdateExtension, 0, FinalizeExtension)
